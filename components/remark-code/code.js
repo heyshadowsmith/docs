c4bb04e709f1d84e2249943250a440fd81d51441
William Li <email@williamli.dev>
2020-07-23 18:19:41 +0800

    reformatting and styling shell commands

--- components/remark-code/code.js
+++ components/remark-code/code.js
@@ -1,50 +1,23 @@
-import refractor from 'refractor'
-import rehype from 'rehype'
 import classNames from 'classnames'
 
 const literal = '`'
 
-const Code = (
-  { className, children, lang, noWrap, color },
-  { darkBg } = {}
-) => {
+const Code = ({ className, children }, { darkBg } = {}) => {
+  // console.log('code >> ', children)
   return (
     <>
-      {lang ? (
-        <code
-          className={classNames(className, {
-            dark: darkBg,
-            'no-wrap': noWrap,
-            [`language-${lang}`]: lang !== undefined
-          })}
-          dangerouslySetInnerHTML={{
-            __html: rehype()
-              .stringify({
-                type: 'root',
-                children: refractor.highlight(children, lang)
-              })
-              .toString()
-          }}
-        />
-      ) : (
-        <code
-          className={classNames(className, {
-            dark: darkBg,
-            'no-wrap': noWrap
-          })}
-        >
-          {children}
-        </code>
-      )}
-      <style jsx global>{`
-        body {
-          background: red;
-        }
-      `}</style>
+      <code
+        className={classNames(className, {
+          dark: darkBg
+          // 'no-wrap': noWrap
+        })}
+      >
+        {children}
+      </code>
       <style jsx>
         {`
         code {
-          color: ${color ? color : '#bd10e0'};
+          color: #bd10e0;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace,
             serif;
@@ -76,6 +49,58 @@ const Code = (
           line-height: 20px;
         }
 
+        :global(pre) code::before {
+          content: none;
+        }
+
+        :global(pre) code::after {
+          content: none;
+        }
+
+        :global(pre) code.language-shell :global(ul),
+        :global(pre) code.language-console :global(ul),
+        :global(pre) code.language-bash :global(ul),
+        :global(pre) code.language-sh :global(ul),
+        :global(pre) code.language-zsh :global(ul) {
+          display: block;
+          margin: 0;
+          padding: 0;
+        }
+
+
+        :global(pre) code.language-shell :global(ul li),
+        :global(pre) code.language-console :global(ul li),
+        :global(pre) code.language-bash :global(ul li),
+        :global(pre) code.language-sh :global(ul li),
+        :global(pre) code.language-zsh :global(ul li) {
+          display: flex;
+          margin: 0;
+          padding: 0;
+          flex-shrink: 0;
+          flex-grow: 0;
+        }
+
+
+        :global(pre) code.language-shell :global(ul li)::before,
+        :global(pre) code.language-console :global(ul li)::before,
+        :global(pre) code.language-bash :global(ul li)::before,
+        :global(pre) code.language-sh :global(ul li)::before,
+        :global(pre) code.language-zsh :global(ul li)::before {
+          content: '$ ';
+          user-select: none;
+          white-space: pre;
+          color: #888;
+          display: block;
+          flex-shrink: 0;
+          flex-grow: 0;
+        }
+
+        
+
+
+
+
+        
         :global(pre).dark {
           border-color: #333;
           background: transparent;
@@ -88,116 +113,7 @@ const Code = (
           color: #50e3c2;
         }
 
-        :global(pre) code::before {
-          content: none;
-        }
-
-        :global(pre) code::after {
-          content: none;
-        }
-
         
-        // highlight.js
-        /*
-
-        github.com style (c) Vasily Polovnyov <vast@whiteants.net>
-
-        */
-
-        code.hljs {
-          display: block;
-          overflow-x: auto;
-          padding: 0.5em;
-          color: #333;
-          background: #f8f8f8;
-        }
-
-        code .hljs-comment,
-        code .hljs-quote {
-          color: #998;
-          font-style: italic;
-        }
-
-        code .hljs-keyword,
-        code .hljs-selector-tag,
-        code .hljs-subst {
-          color: #333;
-          font-weight: bold;
-        }
-
-        code .hljs-number,
-        code .hljs-literal,
-        code .hljs-variable,
-        code .hljs-template-variable,
-        code .hljs-tag .hljs-attr {
-          color: #008080;
-        }
-
-        code :global(.hljs-string),
-        code .hljs-doctag {
-          color: #d14;
-        }
-
-        code .hljs-title,
-        code .hljs-section,
-        code .hljs-selector-id {
-          color: #900;
-          font-weight: bold;
-        }
-
-        code .hljs-subst {
-          font-weight: normal;
-        }
-
-        code .hljs-type,
-        code .hljs-class .hljs-title {
-          color: #458;
-          font-weight: bold;
-        }
-
-        code .hljs-tag,
-        code .hljs-name,
-        code .hljs-attribute {
-          color: #000080;
-          font-weight: normal;
-        }
-
-        code .hljs-regexp,
-        code .hljs-link {
-          color: #009926;
-        }
-
-        code .hljs-symbol,
-        code .hljs-bullet {
-          color: #990073;
-        }
-
-        code .hljs-built_in,
-        code .hljs-builtin-name {
-          color: #0086b3;
-        }
-
-        code .hljs-meta {
-          color: #999;
-          font-weight: bold;
-        }
-
-        code .hljs-deletion {
-          background: #fdd;
-        }
-
-        code .hljs-addition {
-          background: #dfd;
-        }
-
-        code .hljs-emphasis {
-          font-style: italic;
-        }
-
-        code .hljs-strong {
-          font-weight: bold;
-        }
-
       `}
       </style>
     </>
--- components/remark-renderer/remark-renderer.js
+++ components/remark-renderer/remark-renderer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import unified from 'unified'
 import markdown from 'remark-parse'
 import remark2rehype from 'remark-rehype'
 import rehype2react from 'rehype-react'
-import highlight from 'rehype-highlight'
+import rehypePrism from '@mapbox/rehype-prism'
+
 import classNames from 'classnames'
 
 import RemarkNote from '~/components/text/remark-note'
@@ -16,36 +17,70 @@ const RemarkRenderer = ({
   children,
   components
 }) => {
-  const markdownProcessor = unified()
-    .use(markdown)
-    .use(remark2rehype)
-    .use(highlight)
-    .use(rehype2react, {
-      createElement: React.createElement,
-      components
+  const renderMarkdown = md => {
+    const markdownProcessor = unified()
+      .use(markdown)
+      //.use(codeFrontmatter)
+      //.use(() => transformer)
+      // .use(() => transformer)
+      // .use(codeExtra, {
+      //   transform: node => {
+      //     if (node.type === 'code' && ['shell', 'sh', 'bash', 'console', 'zsh'].includes(node.lang)) {
+      //       console.log('node', node)
+
+      //     }
+
+      //     return node
+      //   }
+      // })
+      .use(remark2rehype)
+      .use(rehypePrism)
+      // .use(highlight)
+      .use(rehype2react, {
+        createElement: React.createElement,
+        components
+      })
+
+    return markdownProcessor.processSync(md).result
+  }
+
+  useEffect(() => {
+    const nodes = document.querySelectorAll(
+      'code.language-shell:not(.shell-restyled), code.language-bash:not(.shell-restyled), code.language-sh:not(.shell-restyled), code.language-console:not(.shell-restyled), code.language-zsh:not(.shell-restyled)'
+    )
+    nodes.forEach(n => {
+      console.log('>>', n)
+      n.className = n.className + ' shell-restyled'
+      n.innerHTML = `<ul>${n.innerHTML
+        .split('\n')
+        .map(line => {
+          return line.trim() !== '' ? `<li>${line}</li>` : ``
+        })
+        .join('')}</ul>`
     })
+  }, [])
 
   return contentType === 'default' ? (
-    <>{markdownProcessor.processSync(children).result}</>
+    <>{renderMarkdown(children)}</>
   ) : contentType === 'note' ? (
-    <RemarkNote type="note">
-      {markdownProcessor.processSync(children).result}
-    </RemarkNote>
+    <RemarkNote type="note">{renderMarkdown(children)}</RemarkNote>
   ) : contentType === 'warning' ? (
-    <RemarkNote type="warning">
-      {markdownProcessor.processSync(children).result}
-    </RemarkNote>
+    <RemarkNote type="warning">{renderMarkdown(children)}</RemarkNote>
   ) : contentType === 'caption' ? (
     <RemarkCaption components={{ ...components }}>{children}</RemarkCaption>
   ) : contentType === 'code' ? (
     <>
-      <div className={classNames({ 'allow-copy': allowCopy === true })}>
-        {markdownProcessor.processSync(children).result}
+      <div
+        className={classNames({
+          'allow-copy': allowCopy === true
+        })}
+      >
+        {renderMarkdown(children)}
       </div>
       <RemarkCaption components={{ ...components }}>{caption}</RemarkCaption>
     </>
   ) : contentType === undefined ? (
-    <>{markdownProcessor.processSync(children).result}</>
+    <>{renderMarkdown(children)}</>
   ) : (
     <div style={{ marginLeft: '2rem', fontSize: '0.9rem' }}>
       unsupported <b>markdown</b> contentType type:{' '}
